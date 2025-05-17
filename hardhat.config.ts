793a7ca6c25208a39862b2d8403dce77fc16f8d4
wongzigii <wongzigii@gmail.com>
2025-05-17 23:55:24 +0800

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,9 @@ require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  paths: {
+    sources: "./contracts",
+  },
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL,
